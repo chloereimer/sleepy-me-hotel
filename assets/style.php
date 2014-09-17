d3cca8e72b2949ebd3b527b8efb6316f35f59269
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 15:05:41 -0400

    removes unused code from style.php

--- assets/style.php
+++ assets/style.php
@@ -4,6 +4,3 @@
   require "../application/third_party/scssphp/scss.inc.php";
 
   scss_server::serveFrom($directory);
-
-  // $scss = new scssc();
-  // echo $scss->compile('@import "stylesheets/master"');
