6ea09036638d122920c40c34b065cf9e2e0153fd
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 13:51:40 -0400

    creates header and footer views

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer class="site-footer">
+  <p><strong>StAuthcomp10125:</strong> I Chloe Reimer, 000280557 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.</p>
+</footer>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <title><?php echo isset($title) ? "$title – Sleepy-Me Hotel" : "Sleepy-Me Hotel"; ?></title>
+</head>
+<body>
+<div>
+  <header class="site-header">
+    <h1 class="brand">Sleepy-Me Hotel</h1>
+  </header>
+</div>
+<main class="site-main">
