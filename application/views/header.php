510965685ff43bbd17861b9d91ac2abb7b9576e7
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 15:06:11 -0400

    uses https for google fonts

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no">
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,700,800">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,700,800">
   <link rel="stylesheet" href="<?php echo assets_url('style.php/_master.scss') ?>">
   <title><?php echo isset($title) ? "$title – Sleepy-Me Hotel" : "Sleepy-Me Hotel"; ?></title>
 </head>
