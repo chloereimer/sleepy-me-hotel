673a6df5ae481d3f9e302465367e9426df4572a7
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 14:52:59 -0400

    adds class to SoA

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="site-footer">
-  <p><strong>StAuthcomp10125:</strong> I Chloe Reimer, 000280557 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.</p>
+  <p class="statement-of-authorship"><strong>StAuthcomp10125:</strong> I Chloe Reimer, 000280557 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.</p>
 </footer>
 <script src="<?php echo javascripts_url('modernizr/modernizr.js'); ?>"></script> <!-- yes yes this should be a custom modernizr eventually -->
 <script src="<?php echo javascripts_url('jquery/jquery.min.js'); ?>"></script>
