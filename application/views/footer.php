34804667e5667d6413ae100daca4114e866c881d
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 14:15:05 -0400

    adds javascript assets to footer
    also initializes foundation on the document

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,5 +2,11 @@
 <footer class="site-footer">
   <p><strong>StAuthcomp10125:</strong> I Chloe Reimer, 000280557 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.</p>
 </footer>
+<script src="<?php echo javascripts_url('modernizr/modernizr.js'); ?>"></script> <!-- yes yes this should be a custom modernizr eventually -->
+<script src="<?php echo javascripts_url('jquery/jquery.min.js'); ?>"></script>
+<script src="<?php echo javascripts_url('foundation/foundation.min.js'); ?>"></script> <!-- this should probably be a custom foundation too -->
+<script>
+  $(document).foundation();
+</script>
 </body>
 </html>
