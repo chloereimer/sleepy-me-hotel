e22da1993df926e6e3f446fd3824382125003da7
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-17 14:15:20 -0400

    adds stylesheet link to the header

--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no">
+  <link rel="stylesheet" href="<?php echo assets_url('style.php/_master.scss') ?>">
   <title><?php echo isset($title) ? "$title – Sleepy-Me Hotel" : "Sleepy-Me Hotel"; ?></title>
 </head>
 <body>
