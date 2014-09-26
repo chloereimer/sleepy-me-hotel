064d40de678cc8b6e29ce0bd948b87443e9c078a
Chloe Reimer <chloe.reimer@gmail.com>
2014-09-25 21:11:04 -0400

    implements a form helper
    to be layered on top of codeigniter's built-in form helper, this one is
    specifically geared to work with Foundation, and allows usage in views
    like foundation_form_input( 'username' ).

---
+++ application/helpers/foundation_form_helper.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!function_exists('foundation_form_input'))
+{   
+  function foundation_form_input( $name , $args = array() )
+  {
+
+    $isValid = ( form_error($name) ? false : true );
+    
+    $node  = "<label class='" . ( $isValid ? "" : " error" ) . "' >" . humanize($name);
+
+    if( !empty( $args['as'] ) ){
+
+      switch ($args['as']) {
+
+        case 'collection':
+
+          if( !empty($args['collection']) ){
+            if( $args['allow_blank'] ){
+              $args['collection'] = array_merge( array(" " => " "), $args['collection']);
+            }
+            $node .= form_dropdown( $name, $args['collection'], set_value($name) );
+          }
+
+          break;
+        
+        default:
+          # ...
+          break;
+
+      }
+
+    } else {
+
+      $node .= form_input( $name, set_value($name) );
+
+    }
+
+    $node .= form_error( $name , '<span class="error">', '</span>');
+
+    $node .= "</label>";
+
+    return $node;
+
+  }
+}
