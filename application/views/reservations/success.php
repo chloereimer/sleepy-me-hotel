8a74ee7a478609aae91cc4fbc2213fb199e0bffa
Chloe Reimer <chloe.reimer@gmail.com>
2014-11-11 00:02:12 -0500

    renames download method

--- application/views/reservations/success.php
+++ application/views/reservations/success.php
@@ -2,6 +2,6 @@
   <div class="reservation-summary">
     <h1>Reservation Summary</h1>
     <p>Congratulations! Your reservation has been confirmed. Your card will have been charged $<?php echo $room->rate; ?>.</p>
-    <p><a href="<?php echo site_url('reservations/download_pdf'); ?>">Click here to download a PDF receipt.</a></p>
+    <p><a href="<?php echo site_url('reservations/download_receipt'); ?>">Click here to download a PDF receipt.</a></p>
   </div>
 </section>
