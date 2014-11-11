d92cdd7dac426df968f143562959d60bc749d6f4
Chloe Reimer <chloe.reimer@gmail.com>
2014-11-10 19:02:53 -0500

    finishes up select a room page

--- application/views/reservations/select_a_room.php
+++ application/views/reservations/select_a_room.php
@@ -1,5 +1,19 @@
 <section class="content">
 
+  <ul class="reservation-steps">
+    <li><a href="<?php echo site_url('reservations'); ?>">Select a Date</a></li>
+    <li class="current">Select a Room</li>
+    <li class="tbd">Guest Info & Payment</li>
+    <li class="tbd">Confirmation</li>
+  </ul>
+
+  <div class="reservation-summary">
+    <h1>Reservation Summary</h1>
+    <p><strong>Arrival Date:</strong> <?php echo $startDate; ?></p>
+    <p><strong>Departure Date:</strong> <?php echo $endDate; ?></p>
+    <p class="room_name" style="display: none;"></p>
+  </div>
+
   <h1>Select a Room</h1>
 
   <?php echo form_open( site_url('/reservations/payment'), array( 'class' => 'select-a-date-form' ) ); ?>
@@ -23,7 +37,7 @@
             <td><?php echo $room->name; ?></td>
             <td><?php echo $room->rate; ?></td>
             <td><span class="show-room-info" data-id="<?php echo $room->id; ?>">Info</span></td>
-            <td><?php echo form_radio( array( 'name' => 'room', 'class' => 'select-room-radio'), $room->id); ?></td>
+            <td><?php echo form_radio( array( 'name' => 'room', 'class' => 'select-room-radio', 'data-number' => $room->number, 'data-name' => $room->name ), $room->id); ?></td>
           </tr>
         <?php endforeach; ?>  
       </tbody>
@@ -56,7 +70,16 @@
     ensureRoomIsSelected();
 
     $('.select-room-radio').change(function(){
-      ensureRoomIsSelected();      
+
+      ensureRoomIsSelected();
+
+      if( $('.select-room-radio:checked').length == 1 ){
+        // I didn't think it was really necessary to have ANOTHER ajax call here.
+        // Hopefully the other ajax calls in this lab (through both .get and .load)
+        // are sufficient to demonstrate my understanding.
+        $('.reservation-summary .room_name').html( '<strong>Room:</strong> No. ' + $(this).data('number') + ', ' + $(this).data('name') ).show();
+      }
+
     });
 
   });
