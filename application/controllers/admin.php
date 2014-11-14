f4c2f24a31803403263373342345edd8ee9b2b63
Chloe Reimer <chloe.reimer@gmail.com>
2014-11-13 23:16:37 -0500

    adds ajax calendar to reservation backend

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -4,9 +4,11 @@ class Admin extends CI_Controller {
 
   function __construct()
   {
+
     parent::__construct();
     $this->load->model('Room');
     $this->load->model('Reservation');
+
   }
 
   public function index()
@@ -100,8 +102,59 @@ class Admin extends CI_Controller {
   }
 
   public function index_reservations(){
-
     $this->template->show('/admin/index_reservations');
+  }
+
+  public function reservation_calendar(){
+
+    $options = array();
+
+    $options['show_next_prev'] = true;
+    $options['next_prev_url'] = site_url() . '/admin/reservation_calendar/';
+
+    $options['template'] = '
+      {table_open}<table>{/table_open}
+      {heading_row_start}<tr>{/heading_row_start}
+
+      {heading_previous_cell}<th><span class="previous_link" data-url="{previous_url}">&lt;&lt;</span></th>{/heading_previous_cell}
+      {heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+      {heading_next_cell}<th><span class="next_link" data-url="{next_url}">&gt;&gt;</span></th>{/heading_next_cell}
+
+      {heading_row_end}</tr>{/heading_row_end}
+
+      {week_row_start}<tr>{/week_row_start}
+      {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+      {week_row_end}</tr>{/week_row_end}
+
+      {cal_row_start}<tr>{/cal_row_start}
+      {cal_cell_start}<td>{/cal_cell_start}
+
+      {cal_cell_content}<span>{day}</span>{/cal_cell_content}
+      {cal_cell_content_today}<span class="today">{day}</span>{/cal_cell_content_today}
+
+      {cal_cell_no_content}<span>{day}</span>{/cal_cell_no_content}
+      {cal_cell_no_content_today}<span class="today">{day}</span>{/cal_cell_no_content_today}
+
+      {cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+      {cal_cell_end}</td>{/cal_cell_end}
+      {cal_row_end}</tr>{/cal_row_end}
+
+      {table_close}</table>{/table_close}
+
+    ';
+
+    $this->load->library( 'calendar' , $options );
+
+    // $reservations = $this->Reservation->get_reservations();
+
+    $year = $this->uri->segment(3);
+    $month = $this->uri->segment(4);
+
+    $data = array();
+
+    $args['calendar'] = $this->calendar->generate( $year, $month, $data );
+    $this->load->view( 'admin/reservation_calendar', $args );
 
   }
 
--- application/views/admin/index_reservations.php
+++ application/views/admin/index_reservations.php
@@ -1,3 +1,41 @@
 <section class="content">
   <h1>Reservations</h1>
+
+  <div class="calendar"></div>
 </section>
+
+<script>
+
+  $(document).ready(function(){
+
+    date = new Date();
+
+    $('.calendar').load( '<?php echo site_url("admin/reservation_calendar"); ?>/' + date.getFullYear() + '/' + (date.getMonth() + 1) , function(){
+      $(window).resize();
+    });
+
+    $('.calendar').on('click', '.previous_link,.next_link', function(){
+      $('.calendar').load( $(this).data('url') );
+    });
+
+    $('input[name=arrival_date]').datepicker({
+      minDate: +1,
+      defaultDate: +1,
+      onClose: function(selectedDate) {
+        $('input[name=departure_date]').datepicker('option', 'minDate', selectedDate);
+        checkAvailability();
+      }
+    });
+
+    $('input[name=departure_date]').datepicker({
+      minDate: +1,
+      defaultDate: +1,
+      onClose: function(selectedDate) {
+        $('input[name=arrival_date]').datepicker('option', 'maxDate', selectedDate);
+        checkAvailability();
+      }
+    });
+
+  });
+
+</script>
